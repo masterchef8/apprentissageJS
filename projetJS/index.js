7cda56c65bfb0e5cdbf92fc09a7e57955ed2fb75
Philippe Giraudeau <giraudeau.philip@gmail.com>
2015-06-11 22:50:46 +0200

    Update projetJS

--- projetJS/index.js
+++ projetJS/index.js
@@ -2,18 +2,26 @@
  * Created by Somebody on 03/06/15.
  *
  *
- * Votre application doit permettre de saisir l’URL d’un podcast,
- * d’afficher le contenu de ce podcast sous une forme ergonomique,
- * avec notamment la liste des entrées par ordre chronologique inverse (plus récents en haut),
- * avec la possibilité de lire (dans la même page) le contenu multimédia d’une entrée (mais pas plusieurs à la fois).
- * Autant que possible, l’état de l’application doit se réfléter dans l’URL, afin de pouvoir revenir rapidement sur un état mémorisé.
- * Vous fournirez également une documentation utilisateur minimale, accessible depuis l’application.
+ *       - Votre application doit permettre de saisir l’URL d’un podcast,
+ *       - d’afficher le contenu de ce podcast sous une forme ergonomique,
+ *       - avec notamment la liste des entrées par ordre chronologique inverse (plus récents en haut),
+ *       - avec la possibilité de lire (dans la même page) le contenu multimédia d’une entrée (mais pas plusieurs à la fois).
+ *       - Autant que possible, l’état de l’application doit se réfléter dans l’URL, afin de pouvoir revenir rapidement sur un état mémorisé.
+ *       - Vous fournirez également une documentation utilisateur minimale, accessible depuis l’application.
  *
- * TODO:
+ * ---------------------EN PLUS----------------------------------------------------------------------------------------
+ * TODO: - afficher le contenu de plusieurs podcats
+ * TODO: - programmer une liste de lecture
+ * TODO: - marquer les entrées déjà lues
+ * TODO: - mémoriser l’état dans le navigateur (local storage)
+ * ---------------------ENCORE PLUS------------------------------------------------------------------------------------
+ * TODO: - Transformer l'app JS en méteor
+ * TODO: - CSS avec bootstrap
  *
  */
 
-function addElement(tab, i,title,link,desc,pub,guid ) {
+
+function addTab(tab, i, title, link, desc, pub, guid) {
     /*
 
      [Log] C'est mon boulot 14.05.2015
@@ -25,7 +33,7 @@ function addElement(tab, i,title,link,desc,pub,guid ) {
      */
     var table = document.getElementById(tab);
     var row = table.insertRow(i);
-    var conca = "<audio controls="+"controls"+" preload="+"none"+" src="+guid+"></<audio>";
+    var conca = "<audio controls=" + "controls" + " onplay=" + "checkPlay(this);" + " preload=" + "none" + " src=" + guid + "></<audio>";
     // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
@@ -41,36 +49,64 @@ function addElement(tab, i,title,link,desc,pub,guid ) {
     cell5.innerHTML = pub;
 
 }
+function addHead(channel) {
+    //TODO: addHead
+}
+
+function checkPlay(audio) {
+    /*
+     Check si Un element audio est en train de jouer un son.
+     Si oui => pause sinon play.
+     */
+    var tagsAudio = document.getElementsByTagName('audio');
+    var i = 0;
+    while (i < tagsAudio.length) {
+        if (tagsAudio[i] !== audio && !tagsAudio[i].paused) {
+            tagsAudio[i].pause();
+        }
+        i++;
+    }
+    audio.play();
+}
+function recupXML(xhr) {
+    var root = xhr.responseXML;
+    var channel = root.getElementsByTagName('channel');
+    var item = channel[0].getElementsByTagName('item');
+    for (var i = 0; i < item.length; i++) {
+        var title = item[i].getElementsByTagName('title')[0].textContent;
+        var link = item[i].getElementsByTagName('link')[0].textContent;
+        var desc = item[i].getElementsByTagName('description')[0].textContent;
+        var pub = item[i].getElementsByTagName('pubDate')[0].textContent;
+        var guid = item[i].getElementsByTagName('guid')[0].textContent;
+        addTab("tab", i + 1, title, link, desc, pub, guid);
+    }
+}
 
 
-window.addEventListener("load", function() {
-    document.getElementById("submit")
-        .addEventListener("click", function() {
-            //console.log("clicked");
-            var url = document.getElementById("url").value;
+window.addEventListener("load", function () {
+
+    var inputUrl = document.getElementById("url");
+    if(location.hash != "" && location.hash != undefined){
+        console.log(location.hash.replace("#",""));
+        inputUrl.value = location.hash.replace("#","");
+    }
+
+        document.getElementById("submit")
+        .addEventListener("click", function () {
+
+            console.log("clicked");
+            var url = document.getElementById("url").value.replace("http://", "");
+            location.hash = url;
+
             proxy = "http://cors-anywhere.herokuapp.com";
 
             var xhr = new XMLHttpRequest();
-            xhr.open('GET', proxy+"/"+url, true);
-            xhr.onreadystatechange = function() {
+            xhr.open('GET', proxy + "/" + url, true);
+            xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4) {
-                    //console.log(xhr.status);
+                    console.log(xhr.status);
                     if (xhr.status === 200) {
-
-                        var root = xhr.responseXML;
-                        var channel = root.getElementsByTagName('channel');
-                        var item = channel[0].getElementsByTagName('item');
-                        for(var i = 0; i<item.length;i++){
-                            var title = item[i].getElementsByTagName('title')[0].textContent;
-                            var link = item[i].getElementsByTagName('link')[0].textContent;
-                            var desc = item[i].getElementsByTagName('description')[0].textContent;
-                            var pub = item[i].getElementsByTagName('pubDate')[0].textContent;
-                            var guid = item[i].getElementsByTagName('guid')[0].textContent;
-                            addElement("tab",i+1,title,link,desc,pub,guid);
-                        }
-
-
-
+                        recupXML(xhr);
                     } else {
                         // Error
                     }
@@ -79,5 +115,9 @@ window.addEventListener("load", function() {
             xhr.send();
 
         });
+});
+
+window.addEventListener('hashchange', function(){
+
 });
 
